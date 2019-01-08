a8852a334cc88eed205506622fc299d784e0e4d3
James Dunne <james@thefreshuk.com>
2019-01-08 12:25:04 +0000

    Added documentation to TopicInterface

--- src/TopicInterface.php
+++ src/TopicInterface.php
@@ -6,7 +6,19 @@ use TheFresh\PubSub\Messages\MessageInterface;
 
 interface TopicInterface
 {
+    /**
+     * Publishes a message to this topic
+     *
+     * @param MessageInterface $message The message to publish
+     */
     public function publish(MessageInterface $message): void;
 
+    /**
+     * Subscribes an enpoint to this topic for messages of
+     * the given type.
+     *
+     * @param string $type The type of message
+     * @param string $endpoint The endpoint to subscribe
+     */
     public function subscribe(string $type, string $endpoint): void;
 }
